3404e2ed388932c75c4b87c2459aef0f0e878c32
renanmerlotti <renanmerlotti@gmail.com>
2025-11-17 13:05:27 -0300

    Initial Spring Boot project

---
+++ src/main/java/com/renan/apodascii/ApodAsciiApplication.java
@@ -0,0 +1,13 @@
+package com.renan.apodascii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApodAsciiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApodAsciiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renan/apodascii/ApodAsciiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.apodascii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApodAsciiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
